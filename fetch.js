bab491d67dc66344980ccbb40ec0e6145059a98f
fiixed <fiixed@gmail.com>
2020-10-17 13:25:20 -0400

    first commit

---
+++ fetch.js
@@ -0,0 +1,17 @@
+const fetch = require("node-fetch");
+
+async function fetchUsers() {
+    const apiKey = "This-api-key-is-for-commercial-use-exclusively.Only-entities-with-a-Spotcrime-contract-May-use-this-key.Call-877.410.1607.";
+    const lat = "33.812500";
+    const lon = "-84.424940";
+    const radius = "5"; // this is in miles
+    const res = await fetch(`http://api.spotcrime.com/crimes.json?lat=${lat}&lon=${lon}&radius=${radius}&key=${apiKey}`);
+    const data = await res.json();
+  
+    console.log(data);
+  }
+  
+  fetchUsers().catch(e => {
+    console.log('There has been a problem with your fetch operation: ' + e.message);
+  });
+
